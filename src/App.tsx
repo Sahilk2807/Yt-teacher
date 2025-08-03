7010810296eeba5453afd2fe213538d493b5b49a
Sahilk2807 <kamblesahil2807@gmail.com>
2025-08-03 20:24:40 +0530

    Create src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
+import TermsPage from './pages/TermsPage';
+import ScrollToTop from './components/ScrollToTop';
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen bg-dark">
+      <ScrollToTop />
+      <Header />
+      <main className="flex-grow container mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about-us" element={<AboutPage />} />
+          <Route path="/contact-us" element={<ContactPage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path="/terms-and-conditions" element={<TermsPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
