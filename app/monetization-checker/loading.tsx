2f76005234bd4dd34215684d46b4b2bc61aa62db
Sahilk2807 <kamblesahil2807@gmail.com>
2025-08-03 20:07:16 +0530

    Delete app directory

--- app/monetization-checker/loading.tsx
+++
@@ -1,7 +0,0 @@
-// app/monetization-checker/loading.tsx
-import SkeletonCard from '../components/SkeletonCard';
-
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <SkeletonCard />;
-}
