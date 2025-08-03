c6590ab1b4d2971d6d0b41e4c050a42d6a94d0cc
Sahilk2807 <kamblesahil2807@gmail.com>
2025-08-03 20:00:11 +0530

    Create app/monetization-checker/loading.tsx

---
+++ app/monetization-checker/loading.tsx
@@ -0,0 +1,7 @@
+// app/monetization-checker/loading.tsx
+import SkeletonCard from '../components/SkeletonCard';
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <SkeletonCard />;
+}
